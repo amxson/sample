faf1bacb3153d66e062a4caa4bd64bc2d31c67d5
Amar_Asfaw <amarasfaw1077@gmail.com>
2024-08-08 15:44:05 +0300

    hey

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -73,7 +73,7 @@ export default function Header() {
       <div className='flex gap-2 md:order-2'>
       <Notification />
         <Button
-          className='w-12 h-10 hidden sm:inline'
+          className='w-12 h-10  sm:inline'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
